1c2d15c9e2843da32d697cac56483b6861fe25f6
Ibrahim <ibrahim@ibrahims-mbp.home>
2024-01-03 11:27:49 -0500

    creating home and create post frontEnd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src={logo} alt="logo" className="w-10 object-contain" />
       </Link>
 
       <Link
